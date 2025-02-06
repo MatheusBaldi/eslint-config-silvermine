17412b5f885a41eb8cd3ffa0daa1d045f7df9375
Matheus Baldi <matheushbaldi@gmail.com>
2025-02-06 18:01:57 -0300

    config: migrate key-spacing to stylistic

--- partials/base.js
+++ partials/base.js
@@ -107,7 +107,6 @@ module.exports = {
       'no-sync': 'error',
       'camelcase': 'error',
       'consistent-this': [ 'error', 'self' ],
-      'key-spacing': 'error',
       'keyword-spacing': [
          'error',
          {
@@ -224,5 +223,6 @@ module.exports = {
       '@stylistic/comma-style': 'error',
       '@stylistic/computed-property-spacing': 'error',
       '@stylistic/eol-last': 'error',
+      '@stylistic/key-spacing': 'error',
    },
 };
