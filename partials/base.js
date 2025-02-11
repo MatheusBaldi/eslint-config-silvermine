83b2ca0a472fb476c2f3a6903f41f2ac60c08cdb
Matheus Baldi <matheushbaldi@gmail.com>
2025-02-11 16:33:53 -0300

    config: migrate comma-spacing to stylistic

--- partials/base.js
+++ partials/base.js
@@ -124,7 +124,7 @@ module.exports = {
       '@stylistic/array-bracket-spacing': [ 'error', 'always' ],
       '@stylistic/block-spacing': 'error',
       'camelcase': 'error',
-      'comma-spacing': 'error',
+      '@stylistic/comma-spacing': 'error',
       'comma-style': 'error',
       'computed-property-spacing': 'error',
       'consistent-this': [ 'error', 'self' ],
